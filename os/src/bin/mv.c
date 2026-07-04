f05f13f591e79a0fb7b77d7aa54740df01564049
jordycimo <lit4spagit@gmail.com>
2026-07-04 02:34:49 -0400

    rewrite mv

--- os/src/bin/mv.c
+++ os/src/bin/mv.c
@@ -1,33 +1,28 @@
 #include <stdio.h>
-#include <errno.h>
 #include <unistd.h>
-#include <string.h>
 
 int main(int argc, char* argv[]) {
-    int mode = 0; // default mode 0: relative pathing; 1 is exact pathing
-    int force = 0; // default force 0: dont overwrite; 1 to force writing
 
-    if (argv[1] == NULL || argv[2] == NULL) {
-        printf("mv: expected 2 arguments, \"mv [source] [dest]\"");
-        return 1;
-    }
+    int force = 0;
+    int verbose = 0;
 
-    if (strcmp(args[1], "-f") == 0) {
-        force = 1;
-    }
+    const char* short_opts = "fv";
 
-    if (mode == 0) {
-        if (access(arg[2], F_OK)) {
-            perror("destination exists! -f to force");
-            return 1;
-        }
+    int opt;
 
-        if (rename(argv[1], argv[2])) {
-            perror("rename");
-
-        } else {
-            printf("%s -> ", argv[1]);
-            printf("%s\n", argv[2]);
+    while((opt = getopt(argc, argv, short_opts)) != -1) {
+        switch (opt) {
+            case 'f':
+                force = 1;
+            break;
+            case 'v':
+                verbose = 1;
+            break;
+            optind++;
         }
     }
+
+    if ()
+
+    return 0;
 }
