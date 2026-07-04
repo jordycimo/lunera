115b5c3f525237e128c52ae16ec262d9a02a106b
jordycimo <lit4spagit@gmail.com>
2026-07-04 17:07:56 -0400

    still rewriting mv

--- os/src/bin/mv.c
+++ os/src/bin/mv.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
 
 int main(int argc, char* argv[]) {
 
     int force = 0;
     int verbose = 0;
 
-    const char* short_opts = "fv";
-
     int opt;
 
-    while((opt = getopt(argc, argv, short_opts)) != -1) {
+    // just parse args here, set option variables
+    while((opt = getopt(argc, argv, "fv")) != -1) {
         switch (opt) {
             case 'f':
                 force = 1;
@@ -18,11 +19,38 @@ int main(int argc, char* argv[]) {
             case 'v':
                 verbose = 1;
             break;
-            optind++;
         }
     }
 
-    if ()
+    // prints force status and all other command line arguments after a hypothetical -f and / or -v
+    if (verbose) {
+        printf("force overwrite: %d\n", force);
+
+        for (int i = optind; i < argc; i++) {
+            printf("%d: %s\n", i, argv[i]);
+        }
+    }
+
+
+    // argc-optind = 2 if the correct amount of arguments were passed
+    if (argc-optind != 2) {
+        fprintf(stderr, "mv: expected at least 2 arguments, \"mv [source] [dest]\"");
+    }
+
+    // if were forcing, ask to confirm and then either write or cancel. also handle errors
+    if (force) {
+        printf("force overwrite? (y/N): \n");
+        int chr = getc(stdin);
+
+        if (chr == 'Y' || chr == 'y') {
+            if (rename(argv[2], argv[3]) != 0) {
+                perror("rename");
+            }
+
+        } else {
+            fprintf(stderr, "mv: operation canceled");
+        }
+    }
 
     return 0;
 }
